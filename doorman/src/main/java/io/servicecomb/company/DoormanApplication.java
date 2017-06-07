50883dd7413940ad65bc33783ee4e6f2b9e65eac
seanyinx <sean.yin@huawei.com>
2017-06-07 17:11:05 +0800

    JAV-56 first commit of company

---
+++ doorman/src/main/java/io/servicecomb/company/DoormanApplication.java
@@ -0,0 +1,12 @@
+package io.servicecomb.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoormanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DoormanApplication.class, args);
+  }
+}
